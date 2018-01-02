9607e015c9be8a5f19c00af27a246cc25aa93de1
masikkk <masi.1989@163.com>
2018-01-02 16:04:28 +0800

    完成344字符串反转

--- 344. Reverse String/Solution.java
+++ 344. Reverse String/Solution.java
@@ -1,5 +1,28 @@
 class Solution {
-    public String reverseString(String s) {
-        
+	//char½»»»
+	public String reverseString(String s){
+		char[] charStr = s.toCharArray();
+		int length = s.length();
+		for(int i=0; i<length/2; i++){
+			char temp = charStr[i];
+			charStr[i] = charStr[length-1-i];
+			charStr[length-1-i] = temp;
+		}
+		String result = new String(charStr);
+		return result;
+	}
+	
+	//Time Limit Exceeded
+    public String reverseString1(String s) {
+        String result="";
+        for(int i=0; i<s.length(); i++){
+        	result = s.charAt(i) + result;
+        }
+        return result;
+    }
+    
+    public static void main(String [] args){
+    	Solution solution = new Solution();
+    	System.out.println(solution.reverseString("Masikkk"));
     }
 }
