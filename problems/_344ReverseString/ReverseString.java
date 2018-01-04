3f4cc550c5487c4c8782a22cbf3ce0e709a92277
masikkk <masi.1989@163.com>
2018-01-04 17:22:17 +0800

    新增657题，添加java文件同名public类

--- problems/_344ReverseString/ReverseString.java
+++ problems/_344ReverseString/ReverseString.java
@@ -23,9 +23,11 @@ class Solution {
         return result;
     }
     
+}
+
+public class ReverseString{
     public static void main(String [] args){
     	Solution solution = new Solution();
     	System.out.println(solution.reverseString("Masikkk"));
     }
 }
-
