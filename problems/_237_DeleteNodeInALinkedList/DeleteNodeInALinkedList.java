1c172bdedb0973b35f79280af8514d8c8af1b5ba
masikkk <masi.1989@163.com>
2018-01-18 21:18:18 +0800

    添加237题

---
+++ problems/_237_DeleteNodeInALinkedList/DeleteNodeInALinkedList.java
@@ -0,0 +1,30 @@
+package _237_DeleteNodeInALinkedList;
+
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode(int x) { val = x; }
+ * }
+ */
+
+class ListNode{
+	int val;
+	ListNode next;
+	ListNode(int x){
+		val = x;
+	}
+}
+
+class Solution {
+    public void deleteNode(ListNode node) {
+        
+    }
+}
+
+public class DeleteNodeInALinkedList {
+	public static void main(String[] args){
+		
+	}
+}
