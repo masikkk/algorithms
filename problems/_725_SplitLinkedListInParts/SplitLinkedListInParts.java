e0d01a55cd19d0d1b4303fef1fdeb2dd71966cba
masikkk <masi.1989@163.com>
2018-01-23 11:03:24 +0800

    添加725题readme

--- problems/_725_SplitLinkedListInParts/SplitLinkedListInParts.java
+++ problems/_725_SplitLinkedListInParts/SplitLinkedListInParts.java
@@ -25,7 +25,10 @@ class Solution {
         	linkedListLength++;
         }
         
+        //返回结果
         ListNode[] result = new ListNode[k];
+        
+        //k>n时，前n个是单节点链表，后面全为null
         if(k >= linkedListLength){
         	int i=0;
         	for(ListNode listNode = root; listNode != null; listNode=listNode.next){
@@ -35,13 +38,15 @@ class Solution {
         	while(i<k){
         		result[i++] = null;
         	}
-        }else{
+        }else{//k<=n时
         	int bigNum = linkedListLength - k * (int)Math.floor(linkedListLength/k);//前边的长段链表个数
         	int bigSize = (int)Math.floor(linkedListLength/k) +1;//每个长段链表的size
         	int littleNum = k - bigNum;//后边的短段链表的个数
         	int littleSize = (int)Math.floor(linkedListLength/k);//短段链表的size
         	int iterator=0;//返回结果的总迭代下标
         	ListNode listNode = root;
+        	
+        	//生成bigNum个长段链表，放到结果数组中
         	for(int i=0; i<bigNum; i++){
         		ListNode prev = null;//此段链表的前驱结点
         		for(int j=0; j<bigSize; listNode=listNode.next,j++){//往后走bigSize个结点
@@ -54,6 +59,8 @@ class Solution {
         			prev = temp;
         		}
         	}
+        	
+        	//生成littleNum个短段链表，放到结果数组中
         	for(int i=0; i<littleNum; i++){
         		ListNode prev = null;//此段链表的前驱结点
         		for(int j=0; j<littleSize; listNode=listNode.next,j++){//往后走littleSize个结点
