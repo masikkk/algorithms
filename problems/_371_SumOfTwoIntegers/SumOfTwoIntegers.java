808bf2e7f216b580ebc79e904e7fedfce136d3d1
masikkk <masi.1989@163.com>
2018-01-30 16:31:26 +0800

    添加371题

---
+++ problems/_371_SumOfTwoIntegers/SumOfTwoIntegers.java
@@ -0,0 +1,21 @@
+package _371_SumOfTwoIntegers;
+
+class Solution {
+    public int getSum(int a, int b) {
+    	int sum = a ^ b;//不考虑进位的和
+        int carry = (a & b)<<1;//进位
+        while(carry!=0){//直到进位为0，结束
+        	int newsum = sum ^ carry;
+        	carry = (sum & carry)<<1;
+        	sum = newsum;
+        }
+        return sum;
+    }
+}
+
+public class SumOfTwoIntegers {
+	public static void main(String[] args){
+		Solution solution = new Solution();
+		System.out.println(solution.getSum(3, 1));
+	}
+}
