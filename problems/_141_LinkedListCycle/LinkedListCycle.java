b20c127935b5bdba78028a11f28fb7d20feb919a
masikkk <masi.1989@163.com>
2018-01-31 17:18:29 +0800

    添加141题

---
+++ problems/_141_LinkedListCycle/LinkedListCycle.java
@@ -0,0 +1,31 @@
+package _141_LinkedListCycle;
+
+/**
+ * Definition for singly-linked list.
+ * class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode(int x) {
+ *         val = x;
+ *         next = null;
+ *     }
+ * }
+ */
+class ListNode{
+	int val;
+	ListNode next;
+	ListNode(int x){
+		val = x;
+	}
+}
+
+class Solution {
+    public boolean hasCycle(ListNode head) {
+        ListNode slow = head;
+        ListNode fast = head;
+    }
+}
+
+public class LinkedListCycle {
+
+}
