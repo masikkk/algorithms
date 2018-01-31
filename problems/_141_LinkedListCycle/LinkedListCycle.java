b232bb768f1c12884f0760f7da0bf5d20463f07b
masikkk <masi.1989@163.com>
2018-01-31 21:21:58 +0800

    做完141题

--- problems/_141_LinkedListCycle/LinkedListCycle.java
+++ problems/_141_LinkedListCycle/LinkedListCycle.java
@@ -23,9 +23,28 @@ class Solution {
     public boolean hasCycle(ListNode head) {
         ListNode slow = head;
         ListNode fast = head;
+        while(slow!=null && fast!=null){
+        	slow = slow.next;
+        	if(fast.next !=null)
+        		fast = fast.next.next;
+        	else
+        		return false;
+        	if(slow==fast)
+        		return true;
+        }
+        return false;
     }
 }
 
 public class LinkedListCycle {
-
+	public static void main(String[] args){
+		ListNode head = new ListNode(1);
+		ListNode second = new ListNode(2);
+		ListNode third = new ListNode(3);
+		head.next = head;
+		second.next = third;
+		third.next = head;
+		Solution solution = new Solution();
+		System.out.println(solution.hasCycle(head));
+	}
 }
