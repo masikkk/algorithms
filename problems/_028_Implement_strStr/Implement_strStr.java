642790d427393173804759c72b2b35777be08c26
masikkk <masi.1989@163.com>
2018-02-25 22:29:31 +0800

    添加028题

---
+++ problems/_028_Implement_strStr/Implement_strStr.java
@@ -0,0 +1,31 @@
+package _028_Implement_strStr;
+
+class Solution {
+    public int strStr(String haystack, String needle) {
+    	if(needle==null || needle.length()==0)
+    		return 0;
+        char[] s = haystack.toCharArray();
+        char[] p = needle.toCharArray();
+        for(int i=0; i<=s.length-p.length;){
+        	for(int j=0; j<p.length;){
+        		if(s[i]==p[j]){
+        			i++;
+        			j++;
+        		}else{
+        			i = i-j+1;
+        			break;
+        		}
+        		if(j==p.length)
+        			return i-j;
+        	}
+        }
+        return -1;
+    }
+}
+
+public class Implement_strStr {
+	public static void main(String[] args){
+		Solution solution = new Solution();
+		System.out.println(solution.strStr("", ""));
+	}
+}
