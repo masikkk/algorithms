1539495370cca1ffac4e026373be5b7fa29f42b3
si.ma <si.ma@nio.com>
2020-01-30 11:45:42 +0800

    543题

--- problems/_543_DiameterOfBinaryTree.java
+++ problems/_543_DiameterOfBinaryTree.java
@@ -3,7 +3,7 @@ package problems;
 import structs.TreeNode;
 
 public class _543_DiameterOfBinaryTree {
-    private static class Solution {
+    private static class SolutionV2018 {
         public int diameterOfBinaryTree(TreeNode root) {
             if (root == null) {
                 return 0;
@@ -27,14 +27,33 @@ public class _543_DiameterOfBinaryTree {
         }
     }
 
+    private static class SolutionV2020 {
+        private int diameter = 0;
+        public int diameterOfBinaryTree(TreeNode root) {
+            diameter = 0;
+            depth(root);
+            return diameter;
+        }
+
+        // 递归计算树深度，计算过程中更新树的直径（左右子树深度之和的最大值）
+        private int depth(TreeNode root) {
+            if (null == root) {
+                return 0;
+            }
+            int left = depth(root.left);
+            int right = depth(root.right);
+            diameter = Math.max(diameter, left + right);
+            return Math.max(left, right) + 1;
+        }
+    }
+
     public static void main(String[] args) {
         String input = "[4,-7,-3,null,null,-9,-3,9,-7,-4,null,6,null,-6,-6,null,null,0,6,5,null,9,null,null,-1,-4,null,null,null,-2]";
-        TreeNode root = TreeNode.stringToTreeNode(input);
-        System.out.println("PreOrderTraverse:");
-        TreeNode.preOrderTraverseRecursive(root);
-        System.out.println();
 
-        Solution solution = new Solution();
-        System.out.println(solution.diameterOfBinaryTree(root));
+        SolutionV2018 solutionV2018 = new SolutionV2018();
+        System.out.println(solutionV2018.diameterOfBinaryTree(TreeNode.stringToTreeNode(input)));
+
+        SolutionV2020 solutionV2020 = new SolutionV2020();
+        System.out.println(solutionV2020.diameterOfBinaryTree(TreeNode.stringToTreeNode(input)));
     }
 }
