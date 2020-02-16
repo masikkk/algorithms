eeac8308b9f74a5efc750f1fdd0bbcc82f2d0c3e
si.ma <si.ma@nio.com>
2020-02-16 23:45:46 +0800

    33题

--- problems/_033_SearchInRotatedSortedArray.java
+++ problems/_033_SearchInRotatedSortedArray.java
@@ -1,7 +1,13 @@
 package problems;
 
+import structs.ListNode;
+
+/**
+ * 在旋转有序数组中搜索
+ * https://leetcode-cn.com/problems/search-in-rotated-sorted-array/
+ */
 public class _033_SearchInRotatedSortedArray {
-    private static class Solution {
+    private static class SolutionV2018 {
         public int search(int[] nums, int target) {
             int low = 0;
             int high = nums.length - 1;
@@ -28,9 +34,52 @@ public class _033_SearchInRotatedSortedArray {
         }
     }
 
+    private static class SolutionV2020 {
+        public int search(int[] nums, int target) {
+            if (null == nums || nums.length == 0) {
+                return -1;
+            }
+            int low = 0, high = nums.length -1;
+
+            while (low <= high) {
+                int mid = (low + high) / 2;
+                if (nums[mid] == target) {
+                    return mid;
+                }
+                if (mid - 1 >= low && nums[low] <= nums[mid - 1]) {
+                    // 左边是有序数组
+                    if (target >= nums[low] && target <= nums[mid - 1]) {
+                        high = mid -1;
+                    } else {
+                        low = mid +1;
+                    }
+                } else {
+                    // 右边是有序数组
+                    if (mid + 1 <= high && target >= nums[mid + 1] && target <= nums[high]) {
+                        low = mid + 1;
+                    } else {
+                        high = mid - 1;
+                    }
+                }
+            }
+            return -1;
+        }
+    }
+
     public static void main(String[] args) {
-        int[] input = {3, 1};
-        Solution solution = new Solution();
-        System.out.println(solution.search(input, 1));
+        SolutionV2018 solutionV2018 = new SolutionV2018();
+        System.out.println("SolutionV2018:");
+        System.out.println(solutionV2018.search(ListNode.stringToIntegerArray("[3, 1]"), 1));
+
+        SolutionV2020 solutionV2020 = new SolutionV2020();
+        System.out.println("SolutionV2020:");
+        System.out.println(solutionV2020.search(ListNode.stringToIntegerArray("[]"), 3));
+        System.out.println(solutionV2020.search(ListNode.stringToIntegerArray("[3]"), 3));
+        System.out.println(solutionV2020.search(ListNode.stringToIntegerArray("[3, 1]"), 1));
+        System.out.println(solutionV2020.search(ListNode.stringToIntegerArray("[3, 1, 2]"), 2));
+        System.out.println(solutionV2020.search(ListNode.stringToIntegerArray("[4,5,6,7,0,1,2]"), 0));
+        System.out.println(solutionV2020.search(ListNode.stringToIntegerArray("[4,5,6,7,0,1,2]"), 3));
+        System.out.println(solutionV2020.search(ListNode.stringToIntegerArray("[6,7,1,2,3,4,5]"), 6));
+        System.out.println(solutionV2020.search(ListNode.stringToIntegerArray("[3,4,5,6,7,8,1,2]"), 2));
     }
 }
