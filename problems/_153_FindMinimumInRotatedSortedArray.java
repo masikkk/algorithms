ccb95acba864cd85a494479c9d0594171577cf29
si.ma <si.ma@nio.com>
2020-02-17 17:57:09 +0800

    153题

--- problems/_153_FindMinimumInRotatedSortedArray.java
+++ problems/_153_FindMinimumInRotatedSortedArray.java
@@ -1,7 +1,13 @@
 package problems;
 
+import structs.ListNode;
+
+/**
+ * 旋转排序数组中的最小值
+ * https://leetcode-cn.com/problems/find-minimum-in-rotated-sorted-array/
+ */
 public class _153_FindMinimumInRotatedSortedArray {
-    private static class Solution {
+    private static class SolutionV2018 {
         public int findMin(int[] nums) {
             int low = 0;
             int high = nums.length - 1;
@@ -22,9 +28,45 @@ public class _153_FindMinimumInRotatedSortedArray {
         }
     }
 
+    private static class SolutionV2020 {
+        public int findMin(int[] nums) {
+            int low = 0, high = nums.length - 1;
+            while (low <= high) {
+                if (low == high) {
+                    return nums[low];
+                }
+                if (high == low + 1) {
+                    return Math.min(nums[low], nums[high]);
+                }
+                int mid = (low + high) / 2;
+                if (nums[mid] < nums[mid - 1]) {
+                    return nums[mid];
+                }
+                if (nums[low] <= nums[mid] && nums[mid] <= nums[high]) {
+                    // 左右都有序
+                    return nums[low];
+                } else if (nums[low] <= nums[mid]) {
+                    // 左边是有序的，最小值肯定再右边
+                    low = mid + 1;
+                } else {
+                    // 右边是有序的，最小值肯定在左边
+                    high = mid - 1;
+                }
+            }
+            return nums[low];
+        }
+    }
+
     public static void main(String[] args) {
-        int[] input = {3, 1, 2};//易错用例：{2,1},{3,1,2},{2,3,1}
-        Solution solution = new Solution();
-        System.out.println(solution.findMin(input));
+        SolutionV2018 solutionV2018 = new SolutionV2018();
+        System.out.println(solutionV2018.findMin(ListNode.stringToIntegerArray("[3, 1, 2]")));
+
+        SolutionV2020 solutionV2020 = new SolutionV2020();
+        System.out.println(solutionV2020.findMin(ListNode.stringToIntegerArray("[1]")));
+        System.out.println(solutionV2020.findMin(ListNode.stringToIntegerArray("[7, 8]")));
+        System.out.println(solutionV2020.findMin(ListNode.stringToIntegerArray("[8, 7]")));
+        System.out.println(solutionV2020.findMin(ListNode.stringToIntegerArray("[5, 6, 4]")));
+        System.out.println(solutionV2020.findMin(ListNode.stringToIntegerArray("[6, 4, 5]")));
+        System.out.println(solutionV2020.findMin(ListNode.stringToIntegerArray("[4,5,6,7,0,1,2]")));
     }
 }
