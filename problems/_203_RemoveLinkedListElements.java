9d443b34dc651bcabb87b00760c5440f121a9222
si.ma <si.ma@nio.com>
2020-02-18 13:48:20 +0800

    203题

--- problems/_203_RemoveLinkedListElements.java
+++ problems/_203_RemoveLinkedListElements.java
@@ -2,8 +2,12 @@ package problems;
 
 import structs.ListNode;
 
+/**
+ * 移除链表元素
+ * https://leetcode-cn.com/problems/remove-linked-list-elements/
+ */
 public class _203_RemoveLinkedListElements {
-    private static class Solution {
+    private static class SolutionV2018 {
         public ListNode removeElements(ListNode head, int val) {
             ListNode fakeHead = new ListNode(0);//值无用的头结点
             fakeHead.next = head;
@@ -21,13 +25,34 @@ public class _203_RemoveLinkedListElements {
         }
     }
 
+    private static class SolutionV2020 {
+        public ListNode removeElements(ListNode head, int val) {
+            if (null == head) {
+                return head;
+            }
+            ListNode newHead = new ListNode(0);
+            newHead.next = head;
+            ListNode pre = newHead, cur = head;
+            while (null != cur) {
+                if (cur.val == val) {
+                    pre.next = cur.next;
+                } else {
+                    pre = cur;
+                }
+                cur = cur.next;
+            }
+            return newHead.next;
+        }
+    }
+
     public static void main(String[] args) {
-        String input = "[6,6,6,6,6]";
-        ListNode inputList = ListNode.stringToListNode(input);
-        System.out.println(ListNode.listNodeToString(inputList));
-        Solution solution = new Solution();
-        ListNode outputList = solution.removeElements(inputList, 6);
-        System.out.println(ListNode.listNodeToString(inputList));
-        System.out.println(ListNode.listNodeToString(outputList));
+        ListNode list = ListNode.stringToListNode("[6,6,6,6,6]");
+        SolutionV2018 solutionV2018 = new SolutionV2018();
+        System.out.println(ListNode.listNodeToString(solutionV2018.removeElements(list, 6)));
+
+        SolutionV2020 solutionV2020 = new SolutionV2020();
+        ListNode list2 = ListNode.stringToListNode("[1,2,1]");
+        System.out.println(ListNode.listNodeToString(solutionV2020.removeElements(list, 6)));
+        System.out.println(ListNode.listNodeToString(solutionV2020.removeElements(list2, 1)));
     }
 }
