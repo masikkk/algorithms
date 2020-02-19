42451c5335f218d3ec516cb07e275352b096ff07
si.ma <si.ma@nio.com>
2020-02-19 12:23:36 +0800

    26题

--- problems/_026_RemoveDuplicatesFromSortedArray.java
+++ problems/_026_RemoveDuplicatesFromSortedArray.java
@@ -1,7 +1,13 @@
 package problems;
 
+import java.util.Arrays;
+
+/**
+ * 删除排序数组中的重复项
+ * https://leetcode-cn.com/problems/remove-duplicates-from-sorted-array/
+ */
 public class _026_RemoveDuplicatesFromSortedArray {
-    private static class Solution {
+    private static class SolutionV2018 {
         public int removeDuplicates(int[] nums) {
             if (nums == null || nums.length == 0) {
                 return 0;
@@ -19,12 +25,39 @@ public class _026_RemoveDuplicatesFromSortedArray {
         }
     }
 
-    public static void main(String[] args) {
-        int[] input = {1, 1, 1, 2, 33, 44, 44};
-        Solution solution = new Solution();
-        System.out.println(solution.removeDuplicates(input));
-        for (int a : input) {
-            System.out.print(a + " ");
+    private static class SolutionV2020 {
+        public int removeDuplicates(int[] nums) {
+            if (null == nums) {
+                return 0;
+            }
+            if (nums.length < 2) {
+                return nums.length;
+            }
+            int left = 0;
+            for (int right = 1; right < nums.length; right++) {
+                if (nums[right - 1] != nums[right]) {
+                    nums[++left] = nums[right];
+                }
+            }
+            return left + 1;
         }
+    }
+
+    public static void main(String[] args) {
+        SolutionV2018 solutionV2018 = new SolutionV2018();
+        System.out.println("SolutionV2018:");
+        int[] input = {1, 1, 1, 2, 33, 44, 44};
+        System.out.println(solutionV2018.removeDuplicates(input) + ": " + Arrays.toString(input));
+
+        System.out.println("SolutionV2020:");
+        SolutionV2020 solutionV2020 = new SolutionV2020();
+        int[] input2 = {1, 1, 1, 2, 33, 44, 44};
+        System.out.println(solutionV2020.removeDuplicates(input2) + ": " + Arrays.toString(input2));
+        int[] input3 = {1, 1};
+        System.out.println(solutionV2020.removeDuplicates(input3) + ": " + Arrays.toString(input3));
+        int[] input4 = {1, 2};
+        System.out.println(solutionV2020.removeDuplicates(input4) + ": " + Arrays.toString(input4));
+        int[] input5 = {1, 1, 1, 2, 33, 44, 44, 44, 44};
+        System.out.println(solutionV2020.removeDuplicates(input5) + ": " + Arrays.toString(input5));
     }
 }
