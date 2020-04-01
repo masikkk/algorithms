733615ff3a66b81f834a5e3b34d7ea706c24a120
si.ma <si.ma@nio.com>
2020-04-01 12:30:50 +0800

    20

--- leetcode/leetcode/_020_ValidParentheses.java
+++ leetcode/leetcode/_020_ValidParentheses.java
@@ -3,7 +3,7 @@ package leetcode.leetcode;
 import java.util.Stack;
 
 public class _020_ValidParentheses {
-    private static class Solution {
+    private static class SolutionV2018 {
         public boolean isValid(String s) {
             Stack<String> stack = new Stack<String>();
             for (int i = 0; i < s.length(); i++) {
@@ -20,19 +20,19 @@ public class _020_ValidParentheses {
                             break;
                     }
                 } else {
-                    if (stack.isEmpty()) {//��;ջΪ��˵����ƥ��
+                    if (stack.isEmpty()) {//中途栈为空说明不匹配
                         return false;
                     } else if (!stack.pop().equals(s.substring(i, i + 1))) {
                         return false;
                     }
                 }
             }
-            return stack.isEmpty() ? true : false;//���ջΪ�ղ�ƥ��
+            return stack.isEmpty() ? true : false;//最后栈为空才匹配
         }
     }
 
     public static void main(String[] args) {
-        Solution solution = new Solution();
-        System.out.println(solution.isValid("[(){}]]"));
+        SolutionV2018 solutionV2018 = new SolutionV2018();
+        System.out.println(solutionV2018.isValid("[(){}]]"));
     }
 }
