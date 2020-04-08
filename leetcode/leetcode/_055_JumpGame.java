cf1585c194b5cf253ad6c3e203291e0b99b07629
si.ma <si.ma@nio.com>
2020-04-08 20:11:26 +0800

    55 超时

--- leetcode/leetcode/_055_JumpGame.java
+++ leetcode/leetcode/_055_JumpGame.java
@@ -1,7 +1,9 @@
 package leetcode.leetcode;
 
+import java.util.HashSet;
 import java.util.LinkedList;
 import java.util.Queue;
+import java.util.Set;
 
 /**
  * 跳跃游戏
@@ -15,19 +17,24 @@ public class _055_JumpGame {
             if (null == nums || nums.length < 2) {
                 return true;
             }
+            Set<Integer> visited = new HashSet<>(nums.length);
             // 队列 queue 存放下次可跳跃到的结点的下标，类似层次遍历
             Queue<Integer> queue = new LinkedList<>();
             queue.offer(0);
 
             while (!queue.isEmpty()) {
                 int current = queue.poll();
+                visited.add(current);
                 // 从当前点可以跳到哪些点
                 for (int i = 1; i <= nums[current]; i++) {
+                    int next = current + i;
                     // 若能直接跳到末尾则结束
-                    if (current + i >= nums.length - 1) {
+                    if (next >= nums.length - 1) {
                         return true;
                     }
-                    queue.offer(current + i);
+                    if (!visited.contains(next)) {
+                        queue.offer(next);
+                    }
                 }
             }
             return false;
