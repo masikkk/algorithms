be3bb38d62a1c137109b923c494b2aa2ae20cc7d
si.ma <si.ma@nio.com>
2020-04-09 17:45:30 +0800

    191

--- leetcode/leetcode/_191_NumberOf1Bits.java
+++ leetcode/leetcode/_191_NumberOf1Bits.java
@@ -1,7 +1,11 @@
 package leetcode.leetcode;
 
+/**
+ * 位1的个数
+ * https://leetcode-cn.com/problems/number-of-1-bits/
+ */
 public class _191_NumberOf1Bits {
-    private static class Solution {
+    private static class SolutionV2018 {
         // you need to treat n as an unsigned value
         public int hammingWeight(int n) {
             long unsignedn = n & 0xFFFFFFFFL; //用long型表示无符号int
@@ -15,9 +19,9 @@ public class _191_NumberOf1Bits {
     }
 
     public static void main(String[] args) {
-        Solution solution = new Solution();
+        SolutionV2018 solutionV2018 = new SolutionV2018();
         //Integer.MAX_VALUE: (2^31)-1, 2147483647
         //Integer.MAX_VALUE+1: -2^31, -2147483648
-        System.out.println(solution.hammingWeight(Integer.MAX_VALUE));//2147483648
+        System.out.println(solutionV2018.hammingWeight(Integer.MAX_VALUE));//2147483648
     }
 }
