99bbdf94de8215b65b84891efe9edba8f0b5a711
si.ma <si.ma@nio.com>
2020-04-09 12:49:27 +0800

    55 倒数第二个用例超时

--- leetcode/leetcode/_055_JumpGame.java
+++ leetcode/leetcode/_055_JumpGame.java
@@ -4,6 +4,7 @@ import java.util.HashSet;
 import java.util.LinkedList;
 import java.util.Queue;
 import java.util.Set;
+import org.junit.jupiter.api.Test;
 
 /**
  * 跳跃游戏
@@ -12,32 +13,47 @@ import java.util.Set;
  * @create 2020-04-08 16:50
  */
 public class _055_JumpGame {
-    private static class SolutionV2020 {
+    private static class SolutionV2020DFS {
         Set<Integer> visited;
         public boolean canJump(int[] nums) {
             if (null == nums || nums.length < 2) {
                 return true;
             }
+            // 已访问过的，无法跳到末尾的结点
             visited = new HashSet<>(nums.length);
             return dfs(nums, 0);
         }
 
+        // 深度优先搜索
         private boolean dfs(int[] nums, int start) {
-            boolean res = false;
             for (int i = 1; i <= nums[start]; i++) {
                 if (start + i >= nums.length - 1) {
                     return true;
                 }
                 if (!visited.contains(start + i)) {
-                    boolean canJump = dfs(nums, start + i);
+                    if(dfs(nums, start + i)) {
+                        return true;
+                    }
                     visited.add(start + i);
-                    res |= canJump;
                 }
             }
-            return res;
+            return false;
         }
+    }
 
-        public boolean canJumpQueue(int[] nums) {
+    @Test
+    public void testSolutionV2020DFS() {
+        SolutionV2020DFS solutionV2020DFS = new SolutionV2020DFS();
+        System.out.println(solutionV2020DFS.canJump(new int[] {2,3,1,1,4}));
+        System.out.println(solutionV2020DFS.canJump(new int[] {3,2,1,0,4}));
+        System.out.println(solutionV2020DFS.canJump(
+                new int[]{1, 2, 2, 6, 3, 6, 1, 8, 9, 4, 7, 6, 5, 6, 8, 2, 6, 1, 3, 6, 6, 6, 3, 2, 4, 9, 4, 5, 9, 8, 2, 2, 1, 6, 1, 6, 2, 2, 6, 1, 8,
+                        6, 8, 3, 2, 8, 5, 8, 0, 1, 4, 8, 7, 9, 0, 3, 9, 4, 8, 0, 2, 2, 5, 5, 8, 6, 3, 1, 0, 2, 4, 9, 8, 4, 4, 2, 3, 2, 2, 5, 5, 9, 3,
+                        2, 8, 5, 8, 9, 1, 6, 2, 5, 9, 9, 3, 9, 7, 6, 0, 7, 8, 7, 8, 8, 3, 5, 0}));
+    }
+
+    private static class SolutionV2020Queue {
+        public boolean canJump(int[] nums) {
             if (null == nums || nums.length < 2) {
                 return true;
             }
@@ -65,18 +81,12 @@ public class _055_JumpGame {
         }
     }
 
-    public static void main(String[] args) {
-        SolutionV2020 solutionV2020 = new SolutionV2020();
-        System.out.println(solutionV2020.canJumpQueue(new int[] {2,3,1,1,4}));
-        System.out.println(solutionV2020.canJumpQueue(new int[] {3,2,1,0,4}));
-        System.out.println(solutionV2020.canJumpQueue(
-                new int[]{1, 2, 2, 6, 3, 6, 1, 8, 9, 4, 7, 6, 5, 6, 8, 2, 6, 1, 3, 6, 6, 6, 3, 2, 4, 9, 4, 5, 9, 8, 2, 2, 1, 6, 1, 6, 2, 2, 6, 1, 8,
-                        6, 8, 3, 2, 8, 5, 8, 0, 1, 4, 8, 7, 9, 0, 3, 9, 4, 8, 0, 2, 2, 5, 5, 8, 6, 3, 1, 0, 2, 4, 9, 8, 4, 4, 2, 3, 2, 2, 5, 5, 9, 3,
-                        2, 8, 5, 8, 9, 1, 6, 2, 5, 9, 9, 3, 9, 7, 6, 0, 7, 8, 7, 8, 8, 3, 5, 0}));
-
-        System.out.println(solutionV2020.canJump(new int[] {2,3,1,1,4}));
-        System.out.println(solutionV2020.canJump(new int[] {3,2,1,0,4}));
-        System.out.println(solutionV2020.canJump(
+    @Test
+    public void testSolutionV2020Queue() {
+        SolutionV2020Queue solutionV2020Queue = new SolutionV2020Queue();
+        System.out.println(solutionV2020Queue.canJump(new int[] {2,3,1,1,4}));
+        System.out.println(solutionV2020Queue.canJump(new int[] {3,2,1,0,4}));
+        System.out.println(solutionV2020Queue.canJump(
                 new int[]{1, 2, 2, 6, 3, 6, 1, 8, 9, 4, 7, 6, 5, 6, 8, 2, 6, 1, 3, 6, 6, 6, 3, 2, 4, 9, 4, 5, 9, 8, 2, 2, 1, 6, 1, 6, 2, 2, 6, 1, 8,
                         6, 8, 3, 2, 8, 5, 8, 0, 1, 4, 8, 7, 9, 0, 3, 9, 4, 8, 0, 2, 2, 5, 5, 8, 6, 3, 1, 0, 2, 4, 9, 8, 4, 4, 2, 3, 2, 2, 5, 5, 9, 3,
                         2, 8, 5, 8, 9, 1, 6, 2, 5, 9, 9, 3, 9, 7, 6, 0, 7, 8, 7, 8, 8, 3, 5, 0}));
