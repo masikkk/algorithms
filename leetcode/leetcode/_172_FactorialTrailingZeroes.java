b9aca1c95eac9a2561b7a56c4cd067390c193bb6
si.ma <si.ma@nio.com>
2020-04-12 23:40:58 +0800

    172

--- leetcode/leetcode/_172_FactorialTrailingZeroes.java
+++ leetcode/leetcode/_172_FactorialTrailingZeroes.java
@@ -8,6 +8,7 @@ package leetcode.leetcode;
 public class _172_FactorialTrailingZeroes {
     private static class SolutionV2020 {
         public int trailingZeroes(int n) {
+            // five 必须用 long，否则随着 five 不断增大会溢出 int 导致变为负值，进而多统计
             long five = 5;
             int zeroCount = 0;
             while (five <= n) {
@@ -17,6 +18,7 @@ public class _172_FactorialTrailingZeroes {
             return zeroCount;
         }
 
+        // 改进方法，n 不断除以 5 变小，没有溢出风险
         public int trailingZeroes2(int n) {
             int zeroCount = 0;
             while (n != 0) {
@@ -40,7 +42,6 @@ public class _172_FactorialTrailingZeroes {
         System.out.println(solutionV2020.trailingZeroes2(5));
         System.out.println(solutionV2020.trailingZeroes2(11));
         System.out.println(solutionV2020.trailingZeroes2(25));
-        // 易错用例，正确结果是 452137076， 如果 five 变量用 int 会溢出变为负值导致多统计了2个5得到错误结果 452137078
         System.out.println(solutionV2020.trailingZeroes2(1808548329));
     }
 }
