b265124b547fda7fb4a661b736f3d49e8f2f213c
si.ma <si.ma@nio.com>
2020-04-17 19:41:52 +0800

    55

--- leetcode/leetcode/_055_JumpGame.java
+++ leetcode/leetcode/_055_JumpGame.java
@@ -14,9 +14,35 @@ import org.junit.jupiter.api.Test;
  */
 public class _055_JumpGame {
     private static class SolutionV2020Greedy {
-
+        public boolean canJump(int[] nums) {
+            if (null == nums || nums.length < 2) {
+                return true;
+            }
+            int max = 0;
+            for (int i = 0; i < nums.length; i++) {
+                if (i > max) {
+                    return false;
+                }
+                max = Math.max(max, i + nums[i]);
+                if (max >= nums.length - 1) {
+                    return true;
+                }
+            }
+            return false;
+        }
     }
 
+    @Test
+    public void testSolutionV2020Greedy() {
+        SolutionV2020Greedy solutionV2020Greedy = new SolutionV2020Greedy();
+        System.out.println(solutionV2020Greedy.canJump(new int[] {2,5,0,0}));
+        System.out.println(solutionV2020Greedy.canJump(new int[] {2,3,1,1,4}));
+        System.out.println(solutionV2020Greedy.canJump(new int[] {3,2,1,0,4}));
+        System.out.println(solutionV2020Greedy.canJump(
+                new int[]{1, 2, 2, 6, 3, 6, 1, 8, 9, 4, 7, 6, 5, 6, 8, 2, 6, 1, 3, 6, 6, 6, 3, 2, 4, 9, 4, 5, 9, 8, 2, 2, 1, 6, 1, 6, 2, 2, 6, 1, 8,
+                        6, 8, 3, 2, 8, 5, 8, 0, 1, 4, 8, 7, 9, 0, 3, 9, 4, 8, 0, 2, 2, 5, 5, 8, 6, 3, 1, 0, 2, 4, 9, 8, 4, 4, 2, 3, 2, 2, 5, 5, 9, 3,
+                        2, 8, 5, 8, 9, 1, 6, 2, 5, 9, 9, 3, 9, 7, 6, 0, 7, 8, 7, 8, 8, 3, 5, 0}));
+    }
 
     // 带备忘录的DF，超时，能通过 74/75 个测试用例S
     private static class SolutionV2020DfsWithMemo {
@@ -144,7 +170,7 @@ public class _055_JumpGame {
     }
 
     @Test
-    public void testSolutionV2020Queue() {
+    public void testSolutionV2020BFS() {
         SolutionV2020BFS solutionV2020BFS = new SolutionV2020BFS();
         System.out.println(solutionV2020BFS.canJump(new int[] {2,3,1,1,4}));
         System.out.println(solutionV2020BFS.canJump(new int[] {3,2,1,0,4}));
