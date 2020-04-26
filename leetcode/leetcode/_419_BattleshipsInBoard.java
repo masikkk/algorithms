e939d8dca06f502fa75facc214d6d701760c3025
si.ma <si.ma@nio.com>
2020-04-26 21:24:46 +0800

    419

--- leetcode/leetcode/_419_BattleshipsInBoard.java
+++ leetcode/leetcode/_419_BattleshipsInBoard.java
@@ -1,31 +1,36 @@
 package leetcode.leetcode;
 
+/**
+ * 甲板上的战舰
+ * https://leetcode-cn.com/problems/battleships-in-a-board/
+ */
 public class _419_BattleshipsInBoard {
-    private static class Solution {
+    private static class SolutionV2018 {
         public int countBattleships(char[][] board) {
             int count = 0;
-			for (int i = 0; i < board.length; i++) {
-				for (int j = 0; j < board[i].length; j++) {
-					if (board[i][j] == 'X') { //新出现x
-						count++; //先加1
-						if (j >= 1 && board[i][j - 1] == 'X')//如果左边是x，不能重复计数，减1
-						{
-							count--;
-						}
-						if (i >= 1 && board[i - 1][j] == 'X')//如果上边是x，不能重复计数，减1
-						{
-							count--;
-						}
-					}
-				}
-			}
+            for (int i = 0; i < board.length; i++) {
+                for (int j = 0; j < board[i].length; j++) {
+                    // 新出现x
+                    if (board[i][j] == 'X') {
+                        count++; //先加1
+                        // 如果左边是x，不能重复计数，减1
+                        if (j >= 1 && board[i][j - 1] == 'X') {
+                            count--;
+                        }
+                        // 如果上边是x，不能重复计数，减1
+                        if (i >= 1 && board[i - 1][j] == 'X') {
+                            count--;
+                        }
+                    }
+                }
+            }
             return count;
         }
     }
 
     public static void main(String[] args) {
-        Solution solution = new Solution();
+        SolutionV2018 solutionV2018 = new SolutionV2018();
         char[][] board = {{'X', '.', '.', 'X'}, {'.', '.', '.', 'X'}, {'.', '.', '.', 'X'}};
-        System.out.println(solution.countBattleships(board));
+        System.out.println(solutionV2018.countBattleships(board));
     }
 }
