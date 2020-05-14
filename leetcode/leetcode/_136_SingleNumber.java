e4ddf50448c431cd2e0758857b2c4f4afacdb77c
si.ma <si.ma@nio.com>
2020-05-14 14:00:58 +0800

    136

--- leetcode/leetcode/_136_SingleNumber.java
+++ leetcode/leetcode/_136_SingleNumber.java
@@ -1,7 +1,13 @@
 package leetcode.leetcode;
 
+import org.junit.jupiter.api.Test;
+
+/**
+ * 只出现一次的数字
+ * https://leetcode-cn.com/problems/single-number/
+ */
 public class _136_SingleNumber {
-    private static class Solution {
+    private static class SolutionV2018 {
         public int singleNumber(int[] nums) {
             int result = nums[0];
             for (int i = 1; i < nums.length; i++) {
@@ -11,9 +17,9 @@ public class _136_SingleNumber {
         }
     }
 
-    public static void main(String[] args) {
-        int[] input = {1, 2, 3, 4, 5, 4, 3, 2, 1};
-        Solution solution = new Solution();
-        System.out.println(solution.singleNumber(input));
+    @Test
+    public void testSolutionV2018() {
+        SolutionV2018 solutionV2018 = new SolutionV2018();
+        System.out.println(solutionV2018.singleNumber(new int[] {1, 2, 3, 4, 5, 4, 3, 2, 1}));
     }
 }
