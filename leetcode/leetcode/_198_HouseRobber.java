7f9840cd559208976b6831e3042e38f18dd06be3
si.ma <si.ma@nio.com>
2020-05-29 16:51:13 +0800

    198

--- leetcode/leetcode/_198_HouseRobber.java
+++ leetcode/leetcode/_198_HouseRobber.java
@@ -22,12 +22,10 @@ public class _198_HouseRobber {
             int[] dp = new int[nums.length];
             dp[0] = nums[0];
             dp[1] = Math.max(nums[0], nums[1]);
-            int max = Math.max(nums[0], nums[1]);
             for (int i = 2; i < nums.length; i++) {
                 dp[i] = Math.max(dp[i - 2] + nums[i], dp[i - 1]);
-                max = Math.max(max, dp[i]);
             }
-            return max;
+            return dp[nums.length - 1];
         }
     }
 
