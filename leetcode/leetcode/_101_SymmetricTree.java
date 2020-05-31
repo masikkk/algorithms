dfe5328abd687da12371bc0364bc75766dad7ab9
si.ma <si.ma@nio.com>
2020-05-31 23:34:43 +0800

    101

--- leetcode/leetcode/_101_SymmetricTree.java
+++ leetcode/leetcode/_101_SymmetricTree.java
@@ -9,7 +9,7 @@ import structs.TreeNode;
  * @create: 2020-01-17 14:39
  */
 public class _101_SymmetricTree {
-    private static class SolutionV2020 {
+    private static class SolutionV202001 {
         public boolean isSymmetric(TreeNode root) {
             if (null == root) {
                 return true;
@@ -25,30 +25,5 @@ public class _101_SymmetricTree {
                 return root1 == root2;
             }
         }
-    }
-
-    public static void main(String[] args) {
-        String input = "[1,2,2,3,4,4,3]";
-        String input2 = "[1,2,2,null,3,null,3]";
-        // 易错用例，左右子树的 "左根右" "右根左" 遍历完全相同，但不对称
-        String input3 = "[1,2,2,2,null,2]";
-        // 更易错用例
-        String input4 = "[5,4,1,null,1,null,4,2,null,2,null]";
-        TreeNode root = TreeNode.stringToTreeNode(input);
-        TreeNode root2 = TreeNode.stringToTreeNode(input2);
-        TreeNode root3 = TreeNode.stringToTreeNode(input3);
-        TreeNode root4 = TreeNode.stringToTreeNode(input4);
-
-        SolutionV2020 solutionV2020 = new SolutionV2020();
-        System.out.println(solutionV2020.isSymmetric(root));
-        System.out.println();
-
-        System.out.println(solutionV2020.isSymmetric(root2));
-        System.out.println();
-
-        System.out.println(solutionV2020.isSymmetric(root3));
-        System.out.println();
-
-        System.out.println(solutionV2020.isSymmetric(root4));
     }
 }
