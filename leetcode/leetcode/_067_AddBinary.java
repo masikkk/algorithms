b786408a91b4fbd335befc0ddc775d4e9219469b
si.ma <si.ma@nio.com>
2020-06-23 20:52:10 +0800

    067

---
+++ leetcode/leetcode/_067_AddBinary.java
@@ -0,0 +1,33 @@
+package leetcode.leetcode;
+
+/**
+ * 二进制求和
+ * https://leetcode-cn.com/problems/add-binary/
+ * @author masikkk.com 2020-06-23 19:47
+ */
+public class _067_AddBinary {
+    private static class SolutionV202006 {
+        public String addBinary(String a, String b) {
+            StringBuilder sb = new StringBuilder();
+            int i = a.length() - 1, j = b.length() - 1;
+            byte carry = 0; // 进位
+            while (i >= 0 || j >= 0 || carry != 0) {
+                if (i >= 0) {
+                    carry += a.charAt(i--) - '0';
+                }
+                if (j >= 0) {
+                    carry += b.charAt(j--) - '0';
+                }
+                sb.insert(0, String.valueOf(carry & 1));
+                carry >>= 1;
+            }
+            return sb.toString();
+        }
+    }
+
+    public static void main(String[] args) {
+        SolutionV202006 solutionV202006 = new SolutionV202006();
+        System.out.println(solutionV202006.addBinary("11", "1"));
+        System.out.println(solutionV202006.addBinary("1010", "1011"));
+    }
+}
