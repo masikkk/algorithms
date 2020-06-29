34f053646de213b3cb7c96832a2448309a7c5f2f
si.ma <si.ma@nio.com>
2020-06-29 13:57:56 +0800

    215

--- leetcode/leetcode/_215_KthLargestElementInArray.java
+++ leetcode/leetcode/_215_KthLargestElementInArray.java
@@ -1,5 +1,7 @@
 package leetcode.leetcode;
 
+import java.util.PriorityQueue;
+import org.junit.jupiter.api.Test;
 import structs.ListNode;
 
 /**
@@ -9,7 +11,7 @@ import structs.ListNode;
  * @create 2020-02-20 16:27
  */
 public class _215_KthLargestElementInArray {
-    private static class SolutionV2020 {
+    private static class SolutionV202002 {
         public int findKthLargest(int[] nums, int k) {
             int low = 0, high = nums.length -1;
             // 第k大的数在升序排序后数组中的下标
@@ -52,9 +54,30 @@ public class _215_KthLargestElementInArray {
         }
     }
 
-    public static void main(String args[]) {
-        SolutionV2020 solutionV2020 = new SolutionV2020();
-        System.out.println(solutionV2020.findKthLargest(ListNode.stringToIntegerArray("[3,2,1,5,6,4]"), 2));
-        System.out.println(solutionV2020.findKthLargest(ListNode.stringToIntegerArray("[3,2,3,1,2,4,5,5,6]"), 4));
+    @Test
+    public void testSolutionV202002() {
+        SolutionV202002 solutionV202002 = new SolutionV202002();
+        System.out.println(solutionV202002.findKthLargest(ListNode.stringToIntegerArray("[3,2,1,5,6,4]"), 2));
+        System.out.println(solutionV202002.findKthLargest(ListNode.stringToIntegerArray("[3,2,3,1,2,4,5,5,6]"), 4));
+    }
+
+    private static class SolutionV202006 {
+        public int findKthLargest(int[] nums, int k) {
+            PriorityQueue<Integer> priorityQueue = new PriorityQueue<>(); // 默认是最小堆
+            for (int n : nums) {
+                priorityQueue.offer(n);
+                if (priorityQueue.size() > k) {
+                    priorityQueue.poll();
+                }
+            }
+            return priorityQueue.peek();
+        }
+    }
+
+    @Test
+    public void testSolutionV202006() {
+        SolutionV202006 solutionV202006 = new SolutionV202006();
+        System.out.println(solutionV202006.findKthLargest(ListNode.stringToIntegerArray("[3,2,1,5,6,4]"), 2));
+        System.out.println(solutionV202006.findKthLargest(ListNode.stringToIntegerArray("[3,2,3,1,2,4,5,5,6]"), 4));
     }
 }
